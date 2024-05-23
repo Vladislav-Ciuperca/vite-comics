f3603dac012b215132ff70a22b3446a3f473a6dd
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-24 00:37:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components : {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      prova: 'ciao a tutti'
+    }
+  },
+    methods:{
+      // placeholder
+    },
+
+    mounted () {
+      // placehyolder
+    }
+}
+
+</script>
+
+
+<template>
+  <div>vite dice {{prova}}!</div>
+  <HelloWorld/>
+</template>
