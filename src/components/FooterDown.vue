e7a3021bd1ebfb1265c39e44df7d7aa83c352a33
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-05-25 18:08:06 +0200

    footerDown ok

--- src/components/FooterDown.vue
+++ src/components/FooterDown.vue
@@ -16,5 +16,41 @@ export default {
 </script>
 
 <template>
-    <div>qui footer down</div>
+    <div class="mine-bg-darkGrey p-4">
+        <div class="container-lg d-flex justify-content-between">
+            <input type="button" value="SIGN-UP NOW!">
+            <div class="contacts d-flex align-items-center">
+                <span>FOLLOW US</span>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.mine-bg-darkGrey {
+    background: #2E2E2E;
+}
+
+input {
+    border: 2px solid #0282F9;
+    padding: 10px 15px;
+    background: none;
+    color: white;
+}
+
+.contacts * {
+    padding: 0 1rem;
+
+}
+
+span {
+    color: #0282F9;
+    font-weight: 500;
+    font-size: 20px;
+}
+</style>
